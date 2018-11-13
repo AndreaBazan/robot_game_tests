a22dcd73bd3fca26305c395b1205c64ff5dd2efa
Andrea Bazan <andrea.bazan.polanco@gmail.com>
2018-11-13 10:23:23 -0600

    finished square and board models

--- models/board.rb
+++ models/board.rb
@@ -1,3 +1,27 @@
 # Defines the Board where the Robot walks
 class Board
+  def initialize(attr = {})
+    @squares = attr[:squares]
+    @oob = Square.new(:oob)
+  end
+
+  def square_at(position) # [1,1] [3,3], [2,4]
+    row = position[1] - 1
+    column = position[0] - 1
+    if @squares[row] != nil && @squares[row][column] != nil
+      @squares[row][column]
+    else
+      @oob
+    end
+  end
+
+  # def to_s
+    
+  # end
 end
+
+# @squares = [
+#   [1,2,3,4],
+#   [5,6,7,8],
+#   [9,0,1,2]
+# ]
--- models/square.rb
+++ models/square.rb
@@ -1,3 +1,9 @@
 # Models the squares that compose a board
 class Square
+  attr_reader :result, :state
+
+  def initialize(state = :empty)
+    @result = result
+    @state = state
+  end
 end
